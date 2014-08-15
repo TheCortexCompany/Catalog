d3f91c099685d904f89d94bad1bd656a9876b524
Rodrigo Ribeiro <T689260@D1928982.bs.br.bsch>
2014-08-15 11:37:40 -0300

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-Cortex MVC Catalog
-==================
-
-Simple MVC Catalog built in PHP for develop tests and training.
--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname( __DIR__ ) . "/application/bootstrap.php";
-require_once 'vendor/autoload.php';
-
-var_dump(
-    Application::run()
-);
-
-
